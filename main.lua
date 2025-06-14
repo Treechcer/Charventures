ef236552d801fe5037cfafe660f4469fa393e7f2
Treecher <treechcer@gmail.com>
2025-06-14 21:42:00 +0200

    made walking to other rooms possible

--- main.lua
+++ main.lua
@@ -1,28 +1,32 @@
 love = require("love")
 player = require("player")
 
+love.window.setMode(810, 600)
+
 function love.load()
     map = {
-        "###########################",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "#                         #",
-        "###########################",
+        "############################",
+        "#                         ##",
+        "#                         ##",
+        "#                         ##",
+        "#                         ##",
+        "#                         ##",
+        "#                         ##",
+        "#                         ##",
+        "#                         ##",
+        "#                          ",
+        "#                          ",
+        "#                         ##",
+        "#                         ##",
+        "#                         ##",
+        "#                         ##",
+        "#                         ##",
+        "#                         ##",
+        "#                         ##",
+        "#                         ##",
+        "############# ##############",
+        "############# ##############",
+        "#                         ##",
     }
 
     local font = love.graphics.newFont("Press Start 2p - font/PressStart2P-Regular.ttf", 30)
@@ -35,7 +39,7 @@ end
 function love.draw()
     --love.graphics.setFont(love.graphics.newFont(60))
     for index, value in ipairs(map) do
-        love.graphics.print(value, 0, 0 + (index - 1) * 30)
+        love.graphics.print(value, 0 - player.camera.x, 0 + (index - 1) * 30 - player.camera.y)
     end
 
     love.graphics.print("@", player.x, player.y)
@@ -43,9 +47,19 @@ end
 
 function love.update(dt)
 
-    if counter >= 1 then
+    if love.keyboard.isDown("a") and player.canMove then
+        player.move({x = -1, y = 0}, map)
+    elseif love.keyboard.isDown("d") and player.canMove then
+        player.move({x = 1, y = 0}, map)
+    elseif love.keyboard.isDown("w") and player.canMove then
+        player.move({x = 0, y = -1}, map)
+    elseif love.keyboard.isDown("s") and player.canMove then
+        player.move({x = 0, y = 1}, map)
+    end
+
+    if counter >= 0.1 then
         counter = 0
-        player.move({x = 1, y = 1})
+        player.canMove = true
     end
 
     counter = counter + dt
--- player.lua
+++ player.lua
@@ -1,15 +1,65 @@
 height = 30
 player = {
     camera = {
-
+        x = 0,
+        y = 0
     },
     height = height,
     x = height,
     y = height,
+    canMove = true,
+    tilex = 1,
+    tiley = 1
 }
 
-function player.move(m)
+function player.move(m, map)
+    counter = 0
+
     player.x = player.x + (1 * m.x * player.height)
+    player.y = player.y + (1 * m.y * player.height)
+
+    player.tilex = (player.x - player.camera.x) / player.height + 1
+    player.tiley = (player.y + player.camera.y) / player.height + 1
+
+    player.canMove = false
+
+    print("DEBUG: tilex = ", player.tilex)
+    print("DEBUG: tiley = ", player.tiley)
+    print("DEBUG: cam Y = ", player.camera.y)
+    print("DEBUG: y = ", player.y)
+    print("DEBUG: map[player.tiley] = ", map[player.tiley])
+    if string.sub(map[player.tiley], player.tilex, player.tilex) == "#" then
+        player.x = player.x - (1 * m.x * player.height)
+        player.y = player.y - (1 * m.y * player.height)
+        
+        player.tilex = (player.x - player.camera.x) / player.height + 1
+        player.tiley = (player.y + player.camera.y) / player.height + 1
+    end
+
+    if player.x == 810 then
+        player.cameraMove({x = 810, y = 0})
+        player.x = player.x - 810
+        player.tilex = (player.x) / player.height + 1
+    elseif player.x == 0 then
+        player.cameraMove({x = -810, y = 0})
+        player.x = player.x + 810 - player.height
+        player.tilex = (player.x) / player.height + 1
+    end
+
+    if player.y == 600 then
+        player.cameraMove({x = 0, y = 600})
+        player.y = player.y - 600
+        player.tiley = (player.y) / player.height + 1
+    elseif player.y <= 0 then
+        player.cameraMove({x = 0, y = -600})
+        player.y = player.y + 600 - player.height
+        player.tiley = (player.y) / player.height + 1
+    end
+end
+
+function player.cameraMove(px)
+    player.camera.x = player.camera.x + px.x
+    player.camera.y = player.camera.y + px.y
 end
 
 return player
